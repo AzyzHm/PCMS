09bbe67f9ee01d197725a44cb5da919cf5286ec5
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2026-06-06 08:44:21 +0100

    minor fix : alreadyenrolled Exception is fixed

--- exceptions/AlreadyEnrolledException.java
+++ exceptions/AlreadyEnrolledException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class AlreadyEnrolledException extends powerHouseException {
     public AlreadyEnrolledException(String memberName, String activityName) {
-        super("Le membre " + memberName + " est déjà inscrit à l'activité '" + activityName + "'.");
+        super("Vous étes dejà inscrit à l'activité ");
     }
 }
